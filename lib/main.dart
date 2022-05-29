acd424a458ff94bf35bf14c5e46e2271889f92f5
Victor <bezerra.victor15@gmail.com>
2022-05-29 18:09:42 -0300

    Aula 94

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,28 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transactions.map((tr) {
               return Card(
-                child: Text(tr.title),
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15, 
+                      vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString()
+                      ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(tr.title),
+                      Text(tr.date.toString()),
+                    ],
+                  )
+                ]),
               );
             }).toList(),
           ),
