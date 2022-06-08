8a510b4f10742685595dd8e9bc0cd8cf46bd036d
Victor <bezerra.victor15@gmail.com>
2022-06-08 15:39:01 -0300

    Aula 124

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -44,7 +44,7 @@ class Chart extends StatelessWidget {
           return ChartBar(
             label: tr['day'] as String,
             value: tr['value'] as double,
-            percentage: 0,
+            percentage: 0.3,
           );
         }).toList(),
       ),
--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -18,10 +18,33 @@ class ChartBar extends StatelessWidget {
       children: [
         Text('R\$${value!.toStringAsFixed(2)}'),
         const SizedBox(height: 5),
-        const SizedBox(
+        SizedBox(
           height: 60,
           width: 10,
-          child: null,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
         const SizedBox(height: 5),
         Text(label!),
